66b615004dc426258b7663295fad4e2048f58217
linx <lincoln.le7@gmail.com>
2020-08-13 11:57:26 +1000

    done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  const { register, handleSubmit, watch, errors } = useForm({
-    mode: "onBlur"
+  const { register, handleSubmit, errors } = useForm({
+    mode: "all"
   });
+
+  const [status, setStatus] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
 
   const campignUUid = "46aa3270-d2ee-11ea-a9f0-e9a68ccff42a";
 
@@ -14,13 +17,27 @@ function App() {
       data,
       campaignUuid: campignUUid
     };
-    console.log(body);
+
+    setSubmitting(true);
 
     const response = await fetcher("https://api.raisely.com/v3/signup", body);
 
-    const responseData = await response.json();
+    if (response.status === 400) {
+      setStatus("Error on signup, check your details and try again.");
+    }
 
-    console.log(responseData);
+    if (response.status === 200) {
+      const responseData = await response.json();
+
+      // lets assume this means success
+      if (responseData.data) setStatus("Signup successful, Congrats!");
+    }
+
+    setSubmitting(false);
+    setTimeout(() => {
+      // Clear status after sometime
+      setStatus(null);
+    }, 5000);
   };
 
   const fetcher = async (url, data) => {
@@ -41,49 +58,95 @@ function App() {
       campaignUuid: campignUUid
     };
 
+    // TODO: Check if email is a valid email address
+    // return "Please enter a valid email address;
+
     const response = await fetcher(
       "https://api.raisely.com/v3/check-user",
       body
     );
 
-    const responseData = await response.json();
+    const responseJSON = await response.json();
 
-    return responseData.data.status === "OK";
+    if (responseJSON.data.status === "EXISTS")
+      return "Email already registered.";
+
+    return responseJSON.data.status === "OK";
   };
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <ul>
+      <h1 className="c-title">Signup.</h1>
+      <form className="c-form" onSubmit={handleSubmit(onSubmit)}>
+        <ul className="c-form__fields">
           <li>
-            <label>First name:</label>
-            <input name="firstName" ref={register({ required: true })} />
-            {errors.firstName && <span>This field is required</span>}
-          </li>
-          <li>
-            <label>Last name:</label>
-            <input name="lastName" ref={register({ required: true })} />
-            {errors.lastName && <span>This field is required</span>}
-          </li>
-          <li>
-            <label>Email:</label>
+            <label className="u-hidden">First name:</label>
             <input
+              autoFocus={true}
+              placeholder="First name"
+              invalid={errors.firstName && "true"}
+              name="firstName"
+              ref={register({ required: true })}
+            />
+            {errors.firstName && (
+              <span className="c-error">This field is required</span>
+            )}
+          </li>
+          <li>
+            <label className="u-hidden">Last name:</label>
+            <input
+              placeholder="Last name"
+              invalid={errors.lastName && "true"}
+              name="lastName"
+              ref={register({ required: true })}
+            />
+            {errors.lastName && (
+              <span className="c-error">This field is required</span>
+            )}
+          </li>
+          <li>
+            <label className="u-hidden">Email:</label>
+            <input
+              placeholder="Email"
+              invalid={errors.email && "true"}
               type="email"
               name="email"
               ref={register({ required: true, validate: validateEmail })}
             />
-            {errors.email && <span>email exists!</span>}
+
+            {errors.email && (
+              <span className="c-error">
+                {errors.email.message || "This field is required"}
+              </span>
+            )}
           </li>
           <li>
-            <label>Password:</label>
+            <label className="u-hidden">Password:</label>
             <input
+              placeholder="Password"
+              invalid={errors.password && "true"}
               type="password"
               name="password"
               ref={register({ required: true })}
             />
+            {errors.password && (
+              <span className="c-error">
+                {errors.password.message || "This field is required"}
+              </span>
+            )}
           </li>
         </ul>
-        <input type="submit" />
+
+        <p>
+          <button
+            disabled={submitting || Object.keys(errors).length !== 0}
+            className="c-submit"
+            type="submit"
+          >
+            {submitting ? "Submitting..." : "Submit"}
+          </button>
+        </p>
+        {status && <p>{status}</p>}
       </form>
     </div>
   );
